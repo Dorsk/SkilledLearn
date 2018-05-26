55035fb0b37c3cee2f3b9c4233e2eb5b75090ea0
DorsK <mourajulien@gmail.com>
2018-05-26 19:40:53 +0200

    Add Level & Object

---
+++ SKILLED/Assets/Scripts/CompleteCameraController.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompleteCameraController : MonoBehaviour
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    
+    public Button btnLeft;
+    public Button btnRight; 
+    public Button btnJump;
+    public Button btnBack;
+      
+    private Vector3 offsetL;
+    private Vector3 offsetR;
+    private Vector3 offsetJ;
+    private Vector3 offsetB; 
+     
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+
+        /*offsetB  = btnBack.transform.position - player.transform.position;
+        offsetR = btnBack.transform.position - player.transform.position;
+        offsetL = btnBack.transform.position - player.transform.position;
+        offsetJ = btnBack.transform.position - player.transform.position;*/
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+
+        /*btnBack.transform.position = btnBack.transform.position + offsetB;
+        btnRight.transform.position = btnRight.transform.position + offset;
+        btnLeft.transform.position = btnLeft.transform.position + offset;
+        btnJump.transform.position = btnJump.transform.position + offset;
+        */
+         
+    }
+}
--- SKILLED/Assets/Scripts/Player.cs
+++ SKILLED/Assets/Scripts/Player.cs
@@ -1,26 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : PhysicsObject
 {
 
     public float maxSpeed = 800;
     public float jumpTakeOffSpeed = 800;
+    public Text countText;
+    public Text winText;
 
     private bool moveLeft = false;
     private bool moveRight = false;
 
+    private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-     
+
+    private int count;              //Integer to store the number of pickups collected so far.
+
 
     // Use this for initialization
     void Awake()
     {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-        
+         
+        //Initialize count to zero.
+        count = 0;
+
+        //Initialze winText to a blank string since we haven't won yet at beginning.
+        winText.text = ""; 
+        SetCountText();
+
     }
 
     protected override void ComputeVelocity()
@@ -57,6 +73,34 @@ public class Player : PhysicsObject
         targetVelocity = move * maxSpeed;
     }
 
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUpCoin"))
+        {
+            other.gameObject.SetActive(false);
+            //Add one to the current value of our count variable.
+            count = count + 1;
+
+            //Update the currently displayed count by calling the SetCountText function.
+            SetCountText();
+        }  
+    }
+
+
+    //This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
+    void SetCountText()
+    {
+        //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+        countText.text =  count.ToString() + " / 10";
+        Debug.Log(count.ToString() + " / 10");
+        //Check if we've collected all 12 pickups. If we have...
+        if (count >= 5)
+        {
+            winText.text = "You win!";
+        }
+    }
 
     public void jump()
     {
